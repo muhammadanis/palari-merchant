a43320cec3ace2781c8e402a02cefe7dc40700ba
Muhammad Anis <muhammad.anis@veritrans.co.id>
2015-06-01 14:57:22 +0700

    done register page

--- app/components/register/register.controller.js
+++ app/components/register/register.controller.js
@@ -1,13 +1,29 @@
 var registerApp = angular.module('loginApp', ['ngAnimate']);
 
-registerApp.controller('registerController', function($rootScope, $scope, $http, $location){
-	$scope.regModel = {};
-	$scope.register = function(){
-		//get username and password from model
-		console.log($scope.regModel.username);
-		console.log($scope.regModel.password);
+registerApp.controller('registerController', ['$rootScope', '$scope', '$http', '$location', '$log' , 
+	function($rootScope, $scope, $http, $location, $log){
+		$scope.regModel = {};
+		$scope.$log = $log;
+		$scope.register = function(){
+			//get username and password from model
+			console.log($scope.regModel.username);
+			console.log($scope.regModel.password);			
 
-		//send to api
-		$scope.error = "Please Put username and password";	
+			$http.post(
+				//url
+				phinisiEndpoint + '/merchant/register', 
+				//data
+				{email : $scope.regModel.username, password: $scope.regModel.password},
+				//config
+				{
+					headers :{ 'Content-Type': 'application/json','Accept': 'application/json'}	,				
+				})
+			.success(function(data,status,headers,config){
+				$scope.$log.log(data);				
+			})
+			.error(function(data,status,headers,config){
+				$scope.$log.log(data);
+				$scope.error = data.error;
+			});
 	};
-});
+}]);
