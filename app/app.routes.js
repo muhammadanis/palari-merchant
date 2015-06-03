3d894263e8fe53ab1057290df247cac86fa6cff2
Muhammad Anis <muhammad.anis@veritrans.co.id>
2015-06-03 13:40:18 +0700

    add register app to routes app

--- app/app.routes.js
+++ app/app.routes.js
@@ -1,4 +1,4 @@
-var routesApp = angular.module('routesApp', ['ui.router','loginApp']);
+var routesApp = angular.module('routesApp', ['ui.router','loginApp', 'registerApp']);
 
 routesApp.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {  
 
