ebbfc0a2ea81b5df9ae9e9ce40a54ecdff7e76a2
gifarikautsar <gifari13@gmail.com>
2015-06-04 16:45:17 +0700

    [WIP #95235496] change store detail model

--- app/components/session/home/home.controller.js
+++ app/components/session/home/home.controller.js
@@ -1,36 +1,30 @@
 phinisiApp.controller('detailsController', ['$scope', '$http', '$window', '$log', 
 	function($scope, $http, $window, $log){
 		$scope.storeDetails = {
-			store_name: '',
-			store_logo: '',
-			phone_number: '',
-			address: '',
-			province: '',
-			city: ''
+			merchant_details: {
+				merchant_name : '', 
+				merchant_logo_url : ''
+			}, 
+			merchant_address: {
+				city_id : '', 
+				address : '', 
+				phone_number : ''
+			}
 		}
 		$scope.provinces = [{id:1, name:'Jawa Barat'}, {id:2, name: 'Banten'}];
 		$scope.cities = [{provinceId: 1, cityId: 1, name:'Bandung'}, {provinceId: 1, cityId: 2, name:'Bogor'}, {provinceId: 2, cityId: 3, name:'Tangerang'},];
-		$scope.$log = $log;
 
 		$scope.submit = function(){
 				//get store details from model
-				console.log($window.sessionStorage.token);
-				console.log($scope.storeDetails);
+				$log.debug($window.sessionStorage.token);
+				$log.debug($scope.storeDetails);
 				$http.post(
 					//url
 					phinisiEndpoint + '/merchant/info/create',
 					//data
 					{
-					  token: $window.sessionStorage.token, 
-					  merchant_details: {
-					  	merchant_name : $scope.storeDetails.store_name, 
-					  	merchant_logo_url : $scope.storeDetails.store_logo
-					  }, 
-					  merchant_address: {
-					      city_id : $scope.storeDetails.city, 
-					      address : $scope.storeDetails.address, 
-					      phone_number : $scope.storeDetails.phone_number
-					  }
+					  merchant_details : $scope.storeDetails.merchant_details,
+					  merchant_address : $scope.storeDetails.merchant_address
 					},
 					//config
 					{
@@ -39,15 +33,15 @@ phinisiApp.controller('detailsController', ['$scope', '$http', '$window', '$log'
 				.success(function(data,status,headers,config){
 					if(data.success){
 						if(data.success){
-							console.log('Create store success!');					
+							$log.debug('Create store success!');					
 						}else{
 							$scope.error = data.description;						
 						}
 					}
-					console.log(data);			
+					$log.debug(data);			
 				})
 				.error(function(data,status,headers,config){
-					console.log(data);
+					$log.debug(data);
 					$scope.error = data.error;				
 				});
 			};
