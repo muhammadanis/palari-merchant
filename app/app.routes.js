828806fe4e7ebe7137a079f0342eb9019305ef08
Danny Pranoto <danny.pranoto@veritrans.co.id>
2015-06-04 13:17:02 +0700

    Added merchant.routes.js

--- app/app.routes.js
+++ app/app.routes.js
@@ -4,34 +4,6 @@ routesApp.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', f
 
   $urlRouterProvider.otherwise('/merchant/product');
 
-  $stateProvider
-
-  	.state('merchant', {
-  		url:'/merchant',
-  		templateUrl: 'app/components/session/merchant.html'
-  	})
-
-    .state('merchant.home', {
-    	url: '/home',
-    	templateUrl: 'app/components/session/home/home.html'
-    })
-
-    .state('merchant.editDetails', {
-      url: '/edit-details',
-      templateUrl: 'app/components/session/home/edit-details.html'
-    })
-
-    .state('merchant.product', {
-      url: '/product',
-      templateUrl: 'app/components/session/product/product.html'
-    })
-
-    .state('merchant.addProduct', {
-      url: '/add-product',
-      templateUrl: 'app/components/session/product/add-product.html'
-    })
-
-
   // $locationProvider.html5Mode(true);
 
 }]);
---
+++ app/components/session/merchant.routes.js
@@ -0,0 +1,33 @@
+routesApp.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {  
+
+  $stateProvider
+
+  	.state('merchant', {
+  		url:'/merchant',
+  		templateUrl: 'app/components/session/merchant.html'
+  	})
+
+    .state('merchant.home', {
+    	url: '/home',
+    	templateUrl: 'app/components/session/home/home.html'
+    })
+
+    .state('merchant.editDetails', {
+      url: '/edit-details',
+      templateUrl: 'app/components/session/home/edit-details.html'
+    })
+
+    .state('merchant.product', {
+      url: '/product',
+      templateUrl: 'app/components/session/product/product.html'
+    })
+
+    .state('merchant.addProduct', {
+      url: '/add-product',
+      templateUrl: 'app/components/session/product/add-product.html'
+    })
+
+
+  // $locationProvider.html5Mode(true);
+
+}]);
