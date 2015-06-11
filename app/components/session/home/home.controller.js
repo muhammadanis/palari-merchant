8b2aa139bf4140264041c83eb0c60473d3bc2559
gifarikautsar <gifari13@gmail.com>
2015-06-11 15:14:53 +0700

    Add district field at edit-details page

--- app/components/session/home/home.controller.js
+++ app/components/session/home/home.controller.js
@@ -13,6 +13,8 @@ phinisiApp.controller('detailsController', ['$scope', '$http', '$window', '$log'
 		}
 		$scope.provinces = {};
 		$scope.cities = {};
+		$scope.districts = {};
+
 		$scope.submit = function(){
 				//get store details from model
 				$log.debug($window.sessionStorage.token);
@@ -46,11 +48,9 @@ phinisiApp.controller('detailsController', ['$scope', '$http', '$window', '$log'
 				});
 			};
 
-		$scope.getDetails = function(){
-
-		};
-
 		$scope.getProvinceList = function(){
+			$scope.storeDetails.merchant_address.city_id = '',
+			$scope.storeDetails.merchant_address.district_id = '',
 			$http.get(
 				//url
 				phinisiEndpoint + '/area/province',				
@@ -58,7 +58,6 @@ phinisiApp.controller('detailsController', ['$scope', '$http', '$window', '$log'
 				{
 					headers :{ 'Content-Type': 'application/json','Accept': 'application/json'}	,				
 				}
-
 			)
 			.success(function(data){
 				$scope.provinces = data;
@@ -71,6 +70,7 @@ phinisiApp.controller('detailsController', ['$scope', '$http', '$window', '$log'
 
 		}
 		$scope.getCityList = function (selectedProvince){
+			$scope.storeDetails.merchant_address.district_id = '',
 			$http.get(
 				//url
 				phinisiEndpoint + '/area/city?parent=' + selectedProvince,				
@@ -78,12 +78,29 @@ phinisiApp.controller('detailsController', ['$scope', '$http', '$window', '$log'
 				{
 					headers :{ 'Content-Type': 'application/json','Accept': 'application/json'}	,				
 				}
-
 			)
 			.success(function(data){
 				$scope.cities = data;	
 				$log.debug($scope.cities);
 				$log.debug("Get city list success");
+			})
+			.error(function(data){
+				$scope.error = data.description;				
+			});
+		};
+		$scope.getDistrictList = function (selectedCity){
+			$http.get(
+				//url
+				phinisiEndpoint + '/area/district?parent=' + selectedCity,				
+				//config
+				{
+					headers :{ 'Content-Type': 'application/json','Accept': 'application/json'}	,				
+				}
+			)
+			.success(function(data){
+				$scope.districts = data;	
+				$log.debug($scope.districts);
+				$log.debug("Get district list success");
 			})
 			.error(function(data){
 				$scope.error = data.description;				
