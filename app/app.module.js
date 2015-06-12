2b7c37d20ae044da2ccc005d24e2c6d7bc84c6b3
Danny Pranoto <danny.pranoto@veritrans.co.id>
2015-06-12 09:56:45 +0700

    Removed payment files and migrated to palari-payment

--- app/app.module.js
+++ app/app.module.js
@@ -1,10 +1,8 @@
-var phinisiApp = angular.module('phinisiApp', ['routesApp', 'sessionApp', 'normalApp', 'filterApp', 'formApp']);
-
-
-
 var sessionApp = angular.module('sessionApp',[]);
 var normalApp = angular.module('normalApp',[]);
 
-var paymentApp = angular.module("paymentApp", ['ngAnimate', 'angularPayments', 'ui.router', 'filterApp', 'formApp', 'dropdownApp']);
+var phinisiApp = angular.module('phinisiApp', ['routesApp', 'sessionApp', 'normalApp', 'filterApp', 'formApp']);
+
+
 
 
